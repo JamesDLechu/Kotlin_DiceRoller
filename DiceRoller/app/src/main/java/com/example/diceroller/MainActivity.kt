9e416fb8025f25248bcc28b08f2fcdd26633b9a6
Shiro <jamesdlechu@gmail.com>
2022-06-22 16:47:38 -0500

    Update: Button inner text changed

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import com.example.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding= ActivityMainBinding.inflate(layoutInflater)
+        val root= binding.root
+        root.setVerticalGravity(Gravity.CENTER)
+        setContentView(root)
+
+        val rollButton= binding.rollButton
+        rollButton.text= "Let's Roll!"
     }
 }
