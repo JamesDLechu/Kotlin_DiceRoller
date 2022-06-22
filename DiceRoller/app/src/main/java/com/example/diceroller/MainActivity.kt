6db820b39354a14b501965dc0e1a7dc52545c3ac
Shiro <jamesdlechu@gmail.com>
2022-06-22 17:01:41 -0500

    Update: Number changes dinamically
    On button clicked

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.view.Gravity
 import android.widget.Toast
 import com.example.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding= ActivityMainBinding.inflate(layoutInflater)
+        binding= ActivityMainBinding.inflate(layoutInflater)
         val root= binding.root
         root.setVerticalGravity(Gravity.CENTER)
         setContentView(root)
@@ -18,7 +22,14 @@ class MainActivity : AppCompatActivity() {
         val rollButton= binding.rollButton
         rollButton.text= "Let's Roll!"
         rollButton.setOnClickListener {
-            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText= binding.rollText
+        val randomInt= Random.nextInt(6) + 1
+
+        resultText.text= randomInt.toString()
     }
 }
